21d8d7825c25cbcfe8688c25fec6eee547a3e08f
Antoine Augusti <antoine.augusti@developpement-durable.gouv.fr>
2018-08-01 16:22:45 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+[![Software License](https://img.shields.io/badge/License-MIT-orange.svg?style=flat-square)](https://github.com/entrepreneur-interet-general/open_api_schemas_to_markdown/blob/master/LICENSE.md)
+
+# AVURNAVs CSV
+Ce projet permet d'extraire les AVURNAVs publiés sur les sites des préfectures maritimes en France métropolitaine en CSV.
+
+## Notice
+This software is available under the MIT license and was developed as part of the [Entrepreneur d'Intérêt Général program](https://entrepreneur-interet-general.etalab.gouv.fr) by the French government.
+
+Projet développé dans le cadre du programme « [Entrepreneur d’intérêt général](https://entrepreneur-interet-general.etalab.gouv.fr) ».
---
+++ main.py
@@ -0,0 +1,46 @@
+import json
+import os
+
+import redis as redis_lib
+import requests
+import pandas as pd
+
+REGIONS = ['atlantique', 'manche', 'méditerranée']
+
+r = requests.get(
+    'https://api.heroku.com/apps/avurnav/config-vars',
+    headers={
+        'Accept': 'application/vnd.heroku+json; version=3',
+        'Authorization': 'Bearer ' + os.environ['HEROKU_TOKEN']
+    }
+)
+r.raise_for_status()
+
+redis = redis_lib.utils.from_url(r.json()['REDIS_URL'])
+
+avurnavs = []
+for region in REGIONS:
+    keys = redis.keys(region + ':*')
+    data = list(map(lambda e: json.loads(e), redis.mget(keys)))
+    avurnavs.extend(data)
+
+df = pd.DataFrame(avurnavs)
+df.rename(
+    columns={
+        'content': 'contenu',
+        'premar_region': 'region_prefecture_maritime',
+        'title': 'titre',
+        'valid_from': 'date_debut_vigueur',
+        'valid_until': 'date_fin_vigueur',
+        'number': 'numero_avurnav'
+    },
+    inplace=True
+)
+
+df = df[[
+    'id', 'region_prefecture_maritime', 'numero_avurnav',
+    'latitude', 'longitude', 'titre', 'contenu',
+    'date_debut_vigueur', 'date_fin_vigueur'
+]]
+
+df.to_csv('avurnavs.csv', index=False)
