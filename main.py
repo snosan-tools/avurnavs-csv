507fdf3c673f2f1542447359c475b330e83d9e95
Antoine Augusti <antoine.augusti@gmail.com>
2022-08-26 15:46:54 +0200

    Use JSON file

--- main.py
+++ main.py
@@ -1,55 +1,51 @@
 import json
 import os
+from pathlib import Path
 
-import redis as redis_lib
-import requests
 import pandas as pd
+import requests
 
-REGIONS = ['atlantique', 'manche', 'méditerranée']
+AVURNAVS_FILENAME = "avurnavs.json"
 
-r = requests.get(
-    'https://api.heroku.com/apps/avurnav/config-vars',
-    headers={
-        'Accept': 'application/vnd.heroku+json; version=3',
-        'Authorization': 'Bearer ' + os.environ['HEROKU_TOKEN']
-    }
-)
-r.raise_for_status()
+response = requests.get(os.getenv("AVURNAVS_JSON_URL"))
+response.raise_for_status()
+with open(AVURNAVS_FILENAME, "w") as f:
+    json.dump(response.json(), f)
 
-redis = redis_lib.utils.from_url(r.json()['REDIS_URL'])
-
-avurnavs = []
-for region in REGIONS:
-    keys = redis.keys(region + ':*')
-    data = list(map(lambda e: json.loads(e), redis.mget(keys)))
-    avurnavs.extend(data)
-
-df = pd.DataFrame(avurnavs)
+df = pd.read_json(AVURNAVS_FILENAME)
 df.rename(
     columns={
-        'content': 'contenu',
-        'premar_region': 'region_prefecture_maritime',
-        'title': 'titre',
-        'valid_from': 'date_debut_vigueur',
-        'valid_until': 'date_fin_vigueur',
-        'number': 'numero_avurnav'
+        "content": "contenu",
+        "premar_region": "region_prefecture_maritime",
+        "title": "titre",
+        "valid_from": "date_debut_vigueur",
+        "valid_until": "date_fin_vigueur",
+        "number": "numero_avurnav",
     },
-    inplace=True
+    inplace=True,
 )
 
-df = df[[
-    'id', 'region_prefecture_maritime', 'numero_avurnav',
-    'latitude', 'longitude', 'titre', 'contenu',
-    'date_debut_vigueur', 'date_fin_vigueur'
-]]
+df = df[
+    [
+        "id",
+        "region_prefecture_maritime",
+        "numero_avurnav",
+        "latitude",
+        "longitude",
+        "titre",
+        "contenu",
+        "date_debut_vigueur",
+        "date_fin_vigueur",
+    ]
+]
 
 # We could have duplicates because in the new
 # version of the Préfet maritime websites they
 # don't expose the ID column
 df.drop_duplicates(
-    subset=['region_prefecture_maritime', 'numero_avurnav'],
-    keep='first',
-    inplace=True
+    subset=["region_prefecture_maritime", "numero_avurnav"], keep="first", inplace=True
 )
 
-df.to_csv('avurnavs.csv', index=False, float_format='%.10g')
+df.to_csv("avurnavs.csv", index=False, float_format="%.10g")
+
+Path(AVURNAVS_FILENAME).unlink()
